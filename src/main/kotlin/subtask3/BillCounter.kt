1ea2db0c7e449ee01fa2e1f70142bc7b82c7fc7b
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-03-23 22:34:55 +0300

    feat: add solution for subtask3

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -1,10 +1,13 @@
 package subtask3
 
 class BillCounter {
-
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
-    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
-    }
+    
+fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
+        val AnnPayment = (bill.sum() - bill[k]) / 2
+        val c = (b - AnnPayment)
+        if(AnnPayment == b)
+            return "Bon Appetit"
+        else
+            return c.toString()
+    }   
 }

d6f266c2823a5d471c37fb2a6fd8543c201276c6
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-03-23 22:36:46 +0300

    feat: add solution for subtask4

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,26 @@
 package subtask4
 
 class StringParser {
+    
+fun getResult(inputString: String): Array<String> {
+        val builder = StringBuilder(inputString)
+        val openbrackets = listOf('(', '<', '[')
+        val closebrackets = listOf(')', '>', ']')
+        val res = ArrayList<String>()
+        var last = 0
 
-    // TODO: Complete the following function
-    fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        for (i in builder.length - 1 downTo 0) {
+            if (openbrackets.contains(builder[i])) {
+                for (j in i until builder.length) {
+                    if (builder[j] == closebrackets[openbrackets.indexOf(inputString[i])]){
+                        last = j
+                    builder[last] = '_'
+                    break
+                }
+            }
+            res.add(0, inputString.substring(i + 1, last))
+        }
     }
+    return res.toTypedArray()
+}
 }

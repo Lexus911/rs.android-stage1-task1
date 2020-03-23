fb9a4915c8a02a6d407240e27f3b427bab3d5636
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-03-23 22:38:07 +0300

    feat: add solution for subtask5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,35 @@
 package subtask5
 
 class HighestPalindrome {
+fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
+        val sb = StringBuilder(digitString)
+        var count = 0
+        var kk = k
+        for (i in 0 until n / 2)
+            if (digitString[i] != digitString[n - 1 - i])
+                count++
 
-    // TODO: Complete the following function
-    fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val diff = k - count
+
+        if (diff < 0) {
+            return "-1"
+        } else {
+            for (i in 0 until n / 2) {
+                if (digitString[i] != digitString[n - 1 - i]) {
+                    if (diff > 0 && digitString[i] != '9' && digitString[n - 1 - i] != '9'){
+                        sb.setCharAt(i, '9')
+                        sb.setCharAt((n - 1 - i), '9')
+                    kk -= 2
+                    count--
+                } else {
+                    val max = maxOf(digitString[i], digitString[n - 1 - i])
+                    sb.setCharAt(i, max)
+                    sb.setCharAt((n - 1 - i), max)
+                    count--
+                }
+            }
+        }
+            return sb.toString()
+        }
     }
 }
